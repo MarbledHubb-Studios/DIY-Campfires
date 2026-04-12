7936a541a1b5b97e8d4622fc57d6440b75366cd3
MarbledNull <cyandoezstuff@gmail.com>
2026-04-12 17:58:47 -0500

    4th state

--- src/main/java/com/marbledhubb/diy_campfires/init/ModBlockStateProperties.java
+++ src/main/java/com/marbledhubb/diy_campfires/init/ModBlockStateProperties.java
@@ -4,5 +4,5 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class ModBlockStateProperties {
 
-    public static final IntegerProperty LOG_AMOUNT = IntegerProperty.create("log_amount", 1,3);
+    public static final IntegerProperty LOG_AMOUNT = IntegerProperty.create("log_amount", 1,4);
 }
