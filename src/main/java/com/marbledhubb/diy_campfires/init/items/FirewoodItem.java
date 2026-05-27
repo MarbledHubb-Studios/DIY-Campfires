d6a6058bbc019b48aa40f73d1c844a5e8eaee784
MarbledNull <cyandoezstuff@gmail.com>
2026-05-27 18:30:59 -0500

    fixes

--- src/main/java/com/marbledhubb/diy_campfires/init/items/FirewoodItem.java
+++ src/main/java/com/marbledhubb/diy_campfires/init/items/FirewoodItem.java
@@ -1,17 +1,15 @@
 package com.marbledhubb.diy_campfires.init.items;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
-public class FirewoodItem extends Item {
-    private final Block block;
+public class FirewoodItem extends BlockItem {
 
-    public FirewoodItem(Block block, Item.Properties properties) {
-        super(properties);
-        this.block = block;
+    public FirewoodItem(Block block, Properties properties) {
+        super(block, properties);
     }
 
     @Override
